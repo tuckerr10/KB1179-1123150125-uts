18b59ea4d64195e7985f31d29f97ffebe6b836b6
tuckerr10 <muhfrendy90@gmail.com>
2025-11-07 09:02:26 +0700

    menambah container

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 import 'splash_screen_2.dart' as s2;
 
 class SplashScreen1 extends StatelessWidget {
-  
   const SplashScreen1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const s2.SplashScreen2()),
-            );
-          },
-          child: const Text('Go to Splash Screen 2'),
+        child: Column(
+
+          children: [
+            Spacer(),
+            SizedBox(height: 50),
+            Container(
+              width: 230,
+              height: 230,
+            )
+          ],
         ),
       ),
     );
   }
+  
 }
